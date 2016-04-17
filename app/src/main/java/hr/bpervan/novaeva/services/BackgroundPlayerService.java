45e9ea260798a4f38b7b4824316dcf4071276a1d
bpervan <branimir.pervan@gmail.com>
2016-04-17 23:58:10 +0200

    BackgrounPlayerService progress

--- app/src/main/java/hr/bpervan/novaeva/services/BackgroundPlayerService.java
+++ app/src/main/java/hr/bpervan/novaeva/services/BackgroundPlayerService.java
@@ -11,6 +11,8 @@ import android.os.Message;
 import android.os.Messenger;
 import android.support.annotation.Nullable;
 
+import java.io.IOException;
+
 /**
  * Created by Branimir on 17.4.2016..
  */
@@ -19,16 +21,41 @@ public class BackgroundPlayerService extends Service {
     public static final int MSG_PAUSE = 1;
     public static final int MSG_STOP = 2;
 
-    private final NotificationManager notificationManager;
+    private final NotificationManager notificationManager = (NotificationManager)this.getSystemService(NOTIFICATION_SERVICE);
     private final Messenger messenger = new Messenger(new IncomingMessageHandler());
 
-    private final MediaPlayer mediaPlayer;
+    private final MediaPlayer mediaPlayer = new MediaPlayer();;
 
-    public BackgroundPlayerService(String streamingUrl){
-        this.notificationManager = (NotificationManager)this.getSystemService(NOTIFICATION_SERVICE);
-        this.mediaPlayer = new MediaPlayer();
+
+    @Override
+    public void onCreate(){
         this.mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        String audioUrl = intent.getStringExtra("audioUrl");
+        if(audioUrl == null || audioUrl.isEmpty()){
+            //banana, handle somehow
+        }
+
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.setDataSource(audioUrl);
+            } catch (IOException e){
+                //banana
+            }
+
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+        }
+        return START_STICKY;
     }
 
     @Nullable
@@ -41,12 +68,26 @@ public class BackgroundPlayerService extends Service {
 
         @Override
         public void handleMessage(Message m){
+            MediaPlayer mediaPlayer = BackgroundPlayerService.this.mediaPlayer;
+            if(mediaPlayer == null){
+                return;
+            }
+
             switch (m.what){
                 case MSG_PLAY:
+                    if(!mediaPlayer.isPlaying()){
+                        mediaPlayer.start();
+                    }
                     break;
                 case MSG_PAUSE:
+                    if(mediaPlayer.isPlaying()){
+                        mediaPlayer.pause();
+                    }
                     break;
                 case MSG_STOP:
+                    if(mediaPlayer.isPlaying()){
+                        mediaPlayer.stop();
+                    }
                     break;
             }
         }
