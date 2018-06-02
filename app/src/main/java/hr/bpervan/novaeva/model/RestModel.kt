9dee6582953e51e1b8a81249ac1b37e5bd35a726
vpriscan <vedran.priscan.hr@gmail.com>
2018-06-02 19:42:46 +0200

    add model v3

--- app/src/main/java/hr/bpervan/novaeva/model/RestModel.kt
+++ app/src/main/java/hr/bpervan/novaeva/model/RestModel.kt
@@ -2,10 +2,55 @@ package hr.bpervan.novaeva.model
 
 import com.google.gson.annotations.SerializedName
 
-/**
- * All constructor parameters must have a default value specified
- * so that no-argument constructor is created and GSON invokes it
- */
+open class ContentDto {
+    var id: Long = 1
+    var regionId: Long = 1
+    var created: Long = -1
+    var modified: Long = -1
+    var author: String? = null
+    var text: String? = null
+    var html: String? = null
+    var title: String? = null
+    var description: String? = null
+    var audio: List<AudioDto>? = null
+    var documents: List<DocumentDto>? = null
+    var images: List<LinkDto>? = null
+    var video: List<LinkDto>? = null
+    var links: List<LinkDto>? = null
+    var supercategory: ContentInfoDto? = null
+}
+
+class CategoryDto : ContentDto() {
+    var totalPages: Long = -1
+    var subcategories: List<ContentInfoDto>? = null
+    var content: List<ContentDto>? = null
+}
+
+class ContentInfoDto {
+    var id: Long = -1
+    var title: String? = null
+}
+
+class AudioDto {
+    var id: Long = -1
+    var title: String? = null
+    var link: String? = null
+    var duration: Long = -1
+}
+
+class DocumentDto {
+    var id: Long = -1
+    var title: String? = null
+    var link: String? = null
+    var type: Long = -1
+}
+
+class LinkDto {
+    var id: Long = -1
+    var title: String? = null
+    var link: String? = null
+}
+
 class EvaDirectoryDTO {
     var directoryId: Long = 0
 
