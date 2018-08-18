abbba5a8fc30f3f3ba142c9ec45ac22ade2d8cd3
vpriscan <vedran.priscan.hr@gmail.com>
2018-08-19 00:32:27 +0200

    update options fragment resources; add custom bookmark view

---
+++ app/src/main/java/hr/bpervan/novaeva/views/EvaBookmarkView.kt
@@ -0,0 +1,30 @@
+package hr.bpervan.novaeva.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import hr.bpervan.novaeva.main.R
+
+/**
+ *
+ */
+class EvaBookmarkView(context: Context?, attrs: AttributeSet?) : TextView(context, attrs) {
+    companion object {
+        private val STATE_IS_BOOKMARKED = intArrayOf(R.attr.is_bookmarked)
+    }
+
+    var bookmarked: Boolean = false
+        set(value) {
+            field = value
+            refreshDrawableState()
+        }
+
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        val drawableState = super.onCreateDrawableState(extraSpace + 1)
+        if (bookmarked) {
+            View.mergeDrawableStates(drawableState, STATE_IS_BOOKMARKED)
+        }
+        return drawableState
+    }
+}
